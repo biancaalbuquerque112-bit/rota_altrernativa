861042e6ac5d519597d63b370bffa091005af3cd
biancaalbuquerque112-bit <bianca.albuquerque112@academico.umj.edu.br>
2026-06-07 01:08:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,54 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql2");
+const cookieParser = require("cookie-parser");
+const sequelize = require("./src/config/database");
+
+const authRoutes = require("./src/routes/authRoutes");
+const rotaRoutes = require("./src/routes/rotaRoutes");
+const caronaRoutes = require("./src/routes/caronaRoutes");
 
 const app = express();
 
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+app.use(express.static(__dirname));
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "759012",
-  database: "rota_alternativa"
-});
 
-db.connect((erro) => {
-  if (erro) {
-    console.log("Erro ao conectar:", erro);
-    return;
-  }
-
-  console.log("Conectado ao MySQL");
-});
-
-app.post("/contato", (req, res) => {
-  const { nome, email, telefone, destino, mensagem } = req.body;
-
-  const sql = `
-    INSERT INTO contatos 
-    (nome, email, telefone, destino, mensagem)
-    VALUES (?, ?, ?, ?, ?)
-  `;
-
-  db.query(sql, [nome, email, telefone, destino, mensagem], (erro) => {
-    if (erro) {
-      return res.status(500).json({ erro: "Erro ao salvar no banco" });
-    }
-
-    res.json({ mensagem: "Dados enviados com sucesso!" });
+sequelize.authenticate()
+  .then(() => {
+    console.log("Banco conectado com sucesso!");
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar:", err);
   });
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+
+// Rotas
+app.use("/", authRoutes);
+app.use("/rotas", rotaRoutes);
+app.use("/caronas", caronaRoutes);
+
+app.get("/teste", (req, res) => {
+  res.send("Funcionando!");
 });
+
+
+app.listen(4000, () => {
+  console.log("Servidor rodando em http://localhost:4000");
+});
+
+setInterval(() => {
+  console.log("Servidor ativo...");
+}, 10000);
